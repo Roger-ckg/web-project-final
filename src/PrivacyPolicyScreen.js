0c6b5ececdeaa25e73a530578a689337ab236f1c
Roger <Roger@mac.com>
2021-12-05 23:29:54 -0800

    add vendors, home screen and privacy policy

---
+++ src/PrivacyPolicyScreen.js
@@ -0,0 +1,13 @@
+const PrivacyPolicyScreen = () => {
+    return (
+        <div className="container">
+            <h1>Privacy Policy</h1>
+            <p>[] is public and reviews and comments are immediately viewable and searchable by anyone around the world. We give you non-public ways to communicate on [] too, through private profile and Direct Messages. You can also use [] under a pseudonym if you prefer not to use your name.</p>
+            <p>When you use [], you can choose to share additional information with us like your email address, phone number, address book contacts, and a public profile. We use this information for things like keeping your account secure and showing you more relevant contents.</p>
+            <p>We give you control through your settings to limit the data we collect from you and how we use it, and to control things like account security, marketing preferences, apps that can access your account, and address book contacts you’ve uploaded to []. You can also download information you have shared on [].</p>
+            <p>In addition to information you share with us, we use your reviews and comments, content you’ve read or liked, and other information to determine what topics you’re interested in, your age, the languages you speak, and other signals to show you more relevant content. We give you transparency into that information, and you can modify or correct it at any time.</p>
+            <p>If you have questions about this policy, how we collect or process your personal data, or anything else related to our privacy practices, we want to hear from you. You can contact us at any time.</p>
+        </div>
+    )
+};
+export default PrivacyPolicyScreen;
